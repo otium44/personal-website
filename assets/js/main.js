d1a948d22c7a2b924a00ee46208fcd4ca6451843
otium44 <68956108+otium44@users.noreply.github.com>
2022-08-12 23:39:22 +0300

    upload the main files

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/*=============== SHOW MENU ===============*/
+let inputs = document.querySelectorAll('.contact-input');
+inputs.forEach((e) => {
+  if (e.value !== ''){
+    let labels = document.querySelectorAll('.contact-label');
+    labels.forEach(e => e.style.color = 'white')
+  }
+})
+/*============== MENU HIDDEN ===============*/
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+/*=============== TESTIMONIAL SWIPER ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== PORTFOLIO ITEM FILTER ===============*/
+
+/*=============== THEME/DISPLAY CUSTOMIZATION ===============*/
+
+/*===== FONTS =====*/
+
+/*===== PRIMARY COLORS =====*/
+
+/*===== THEME BACKGROUNDS =====*/
